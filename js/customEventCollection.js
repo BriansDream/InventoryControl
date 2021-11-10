5ba01c502beecfc7fe64b41d767cbe1bb1735ed2
briansdream <48577275+BriansDream@users.noreply.github.com>
2021-11-10 23:19:47 +0700

    Feature/crud (#2)
    * Adding function delete for everydata
    
    * Adding update data feature

---
+++ js/customEventCollection.js
@@ -0,0 +1,10 @@
+
+
+const customEventCloseFormUpdateHandler = () => {
+    const updateFormContainer = document.getElementById('updateform-container');
+    updateFormContainer.setAttribute('hidden','hidden');
+}
+
+
+
+export {customEventCloseFormUpdateHandler};
--- js/storage.js
+++ js/storage.js
@@ -1,5 +1,9 @@
-const cacheKey = 'CACHE_KEY';
 
+import { customEventCloseFormUpdateHandler } from "./customEventCollection.js";
+
+
+
+const cacheKey = 'CACHE_KEY';
 const checkWebStorage = () => {
     return typeof(Storage) != null;
 }
@@ -35,53 +39,129 @@ const showData = () => {
 }
 }
 
+const reloadPageFunct = () => { 
+    return location.reload()
+}
+
 const renderHistoryData = () => {
     const historyData = showData();
  
     const tableBody = document.querySelector('.table-body');
-    
     // Agar tidak terjadi data ganda
     // After data added clean up body table and update using data in storage 
         tableBody.innerHTML = "";
+      
     for(let index=0; index < historyData.length; index++) {
-        
-        // Create row element
+        // Create new element
         const row = document.createElement('tr');
+        const btnUpdate = document.createElement('button');
+        const btnDelete = document.createElement('button');
+        const td = document.createElement('td');
+
         row.innerHTML = `<td> ${index} </td>`;
         row.innerHTML += `<td> ${historyData[index].itemName} </td>`;
         row.innerHTML += `<td> ${historyData[index].itemStock} </td>`;
-       
 
+
+        btnDelete.setAttribute('class','btnDelete');
+        btnDelete.innerHTML = 'Delete';
+        btnDelete.classList.add('btnUpdateDelete');
+        
+        btnUpdate.setAttribute('class','btnUpdate');
+        btnUpdate.innerHTML = 'Update';
+        btnUpdate.classList.add('btnUpdateDelete');
+        
+        // Memasukkan objek create elemen ke dalam HTML
+        td.appendChild(btnDelete);
+        td.insertBefore(btnUpdate,btnDelete);
+        row.appendChild(td);
         tableBody.appendChild(row);
-    }
-}
-
-const updateButton = () => {
-    const btnUpdate = document.createElement('div');
-    btnUpdate.setAttribute('class','btnUpdate');
-    btnUpdate.innerHTML = "Update";
-    btnUpdate.classList.add('btnUpdateDelete');
-    return btnUpdate;
-
-
-}
 
 
 
-// Clear all data from storage
-const btnClearAllData = () => {
-    if(checkWebStorage()) {
-        if(localStorage.getItem(cacheKey) == null) {
-            alert('data belum ada');
-        } else {
-            localStorage.removeItem(cacheKey);
-            renderHistoryData();
-            console.log(showData());
+
+        btnUpdate.addEventListener('click', () => {
+            const updateFormContainer = document.getElementById('updateform-container');
+            const btnCloseUpdate = document.querySelector('.closeUpdate');
+            const formUpdate = document.querySelector('.updateForm');
+            const updateNameItem = document.querySelector('.updateNameItem');
+            const updateStockItem = document.querySelector('.updateStockItem');
+            
+            updateFormContainer.removeAttribute('hidden');
+            
+            if(historyData != '') {
+                const getIdData = historyData[index].id;
+                if(getIdData == historyData[index].id) {
+                  updateNameItem.setAttribute('value', historyData[index].itemName);
+                  updateStockItem.setAttribute('value', historyData[index].itemStock);
+                }
+            }
+
+            formUpdate.addEventListener('submit',() => {
+                    const itemNameUpdate = document.querySelector('.updateNameItem').value;
+                    const itemStockUpdate = document.querySelector('.updateStockItem').value;
+
+                    if(historyData != '') {
+                        const getIdData = historyData[index].id;
+                        if(getIdData == historyData[index].id) {
+                            historyData[index].itemName = itemNameUpdate;
+                            historyData[index].itemStock = itemStockUpdate;
+                            if(checkWebStorage()) {
+                                localStorage.removeItem(cacheKey);
+                                historyData.forEach( dataHistory => {
+                                    putData(dataHistory);
+                                });
+                            }
+                        }
+                    }
+
+                    reloadPageFunct();
+            })
+          
+
+
+            // Custom Event Close form update
+            const customEventCloseFromUpdate = new Event('customEventCloseFormUpdate');
+            btnCloseUpdate.addEventListener('customEventCloseFormUpdate',customEventCloseFormUpdateHandler);
+            btnCloseUpdate.addEventListener('click', () => {
+                // Bangkitkan custom event
+                btnCloseUpdate.dispatchEvent(customEventCloseFromUpdate)
+            });
+
+
+
+
+        })
+
+
+
+        
+        // When delete button clicked, run this funct
+        btnDelete.addEventListener('click', () => {
+        // if data's not empty
+        if(historyData != '') {
+            const getIdData = historyData[index].id; 
+        // Check if id thaw we click == with id in storage 
+           if(getIdData == historyData[index].id) {
+        // Delete based on we've clicked 
+            historyData.splice(index,1);
+        // every transaction with local storage have to check web storage
+                if(checkWebStorage()) {
+                    localStorage.removeItem(cacheKey);
+                    historyData.forEach(data => {
+                        putData(data);
+                    });  
+                }
+                
+           }
+
         }
+        reloadPageFunct();
+
+        });
+
     }
+   
 }
-
-
-
 renderHistoryData();
 
